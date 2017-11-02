7bd5e4cc126f418c3f08864bb73c23db9e335e4d
Henry Cox <henry.cox@manchester.ac.uk>
2017-11-02 20:36:27 +0000

    Sorted out team and opponent goals in gw history

--- Importdata.py
+++ Importdata.py
@@ -293,10 +293,10 @@ def savealldata(filename):
 	# index to select the history data we want
 	index = [7,8,10,11,12,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,
 			30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,
-			48,49,50,53,5,4,3]
+			48,49,50,53,5]
 
 	# set the data names according to our index above
-	fhistnames = [fhisttypes[j] for j in index]
+	fhistnames = [fhisttypes[j] for j in index]+['team_goals','opponent_goals']
 
 	# loop through each week in history adding in the titles
 	for i in range(numhist):
